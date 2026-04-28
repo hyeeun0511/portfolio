7448ab5d36e5f4002e460782397d452a3f4a5212
방혜은 <banghyeeun@banghyeeun-ui-MacBookPro.local>
2026-04-28 15:35:39 +0900

    update

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-document.querySelectorAll('a[href^="#"]').forEach((link)=>{
-    link.addEventListener("click",(e)=>{
-      const target=document.querySelector(link.getAttribute("href"));
-      if(target){
-        e.preventDefault();
-        target.scrollIntoView({behavior:"smooth"});
-      }
-    });
-  });
-// ===== 스크롤 위치에 따라 active 표시 =====
-
 const sections = document.querySelectorAll("section, header");
 const dots = document.querySelectorAll(".side-nav .dot");
 
@@ -17,13 +6,15 @@ window.addEventListener("scroll", () => {
 
   sections.forEach((section) => {
     const sectionTop = section.offsetTop - 150;
-    if (scrollY >= sectionTop) {
+
+    if (window.scrollY >= sectionTop) {
       current = section.getAttribute("id");
     }
   });
 
   dots.forEach((dot) => {
     dot.classList.remove("active");
+
     if (dot.getAttribute("href") === "#" + current) {
       dot.classList.add("active");
     }
