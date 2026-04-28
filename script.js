0ef24d15312d5e89c3ba29e8f642440ce3188738
방혜은 <banghyeeun@banghyeeun-ui-MacBookPro.local>
2026-04-28 14:59:42 +0900

    update

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-const menuBtn = document.getElementById("menuBtn");
-const nav = document.getElementById("nav");
-
-menuBtn.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
-
-const navLinks = document.querySelectorAll(".nav a");
-
-navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    nav.classList.remove("active");
-  });
-});
-
-const sections = document.querySelectorAll(".section");
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("show");
+document.querySelectorAll('a[href^="#"]').forEach((link)=>{
+    link.addEventListener("click",(e)=>{
+      const target=document.querySelector(link.getAttribute("href"));
+      if(target){
+        e.preventDefault();
+        target.scrollIntoView({behavior:"smooth"});
       }
     });
-  },
-  {
-    threshold: 0.18,
-  }
-);
-
-sections.forEach((section) => {
-  observer.observe(section);
-});
+  });
